0a432671299d930b3d6d254331513055e4227336
Fahreza Raharjo <fahrezaraharjo92@gmail.com>
2022-03-03 08:04:01 +0700

    fixed pagenation

--- routes/index.js
+++ routes/index.js
@@ -8,23 +8,23 @@ const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
 
-function isLoggedIn(req, res, next){
-  if(req.session.user) {
+function isLoggedIn(req, res, next) {
+  if (req.session.user) {
     next()
-  }else {
+  } else {
     res.redirect('/login')
   }
 }
 
-router.get('/login', function(req, res){
-  res.render('login', {loginMessage: req.flash('loginMessage')})
+router.get('/login', function (req, res) {
+  res.render('login', { loginMessage: req.flash('loginMessage') })
 })
 
-router.post("/login", function (req, res){
+router.post("/login", function (req, res) {
   const email = req.body.email
-  const password =req.body.password
+  const password = req.body.password
 
-  db.get("select * from user where email = ?",[email],(err, user) =>{ 
+  db.get("select * from user where email = ?", [email], (err, user) => {
     if (err) {
       req.flash('loginMessage', 'Gagal Login')
       return res.redirect("/login")
@@ -33,42 +33,42 @@ router.post("/login", function (req, res){
       req.flash('loginMessage', 'User Tidak DiTemukan')
       return res.redirect("/login")
     }
-    bcrypt.compare(password, user.password, function(err, result) {
-      if(result){
-      req.session.user = user
-      res.redirect("/")
+    bcrypt.compare(password, user.password, function (err, result) {
+      if (result) {
+        req.session.user = user
+        res.redirect("/")
       } else {
         req.flash('loginMessage', 'Password salah')
         return res.redirect("/login")
       }
     });
   });
-      
-  })
+
+})
 
 
-router.get('/register', function(req, res){
+router.get('/register', function (req, res) {
   res.render('register')
 })
 
 
-router.post("/register", function (req, res){
+router.post("/register", function (req, res) {
   const email = req.body.email
   const fullname = req.body.fullname
-  const password =req.body.password
-  
-  
-  bcrypt.hash(password, saltRounds, function(err, hash) {
-  db.run("insert into user (email, password, fullname)values(?, ?, ?)",[email, hash, fullname],(err) =>{
-    if (err) return res.send("register failed")
-    res.redirect("/login")
+  const password = req.body.password
+
+
+  bcrypt.hash(password, saltRounds, function (err, hash) {
+    db.run("insert into user (email, password, fullname)values(?, ?, ?)", [email, hash, fullname], (err) => {
+      if (err) return res.send("register failed")
+      res.redirect("/login")
     });
   })
 })
 
 
-router.get('/logout', function(req, res){
-  req.session.destroy(function(err) {
+router.get('/logout', function (req, res) {
+  req.session.destroy(function (err) {
     res.redirect('/login')
   })
 })
@@ -77,6 +77,7 @@ router.get('/logout', function(req, res){
 
 
 router.get('/', isLoggedIn, function (req, res,) {
+
 
   const url = req.url == "/" ? "/?page=1" : req.url
 
@@ -90,32 +91,28 @@ router.get('/', isLoggedIn, function (req, res,) {
     params.push(`complete = ${req.query.complete}`)
   }
 
-
   const page = req.query.page || 1
   const limit = 3
   const offset = (page - 1) * limit
   let sql = `select count(*) as total from todo`;
   if (params.length > 0) {
-    sql += ` where ${params.join('and')}`
+    sql += ` where ${params.join(' and ')}`
   }
-
   db.get(sql, (err, row) => {
     const pages = Math.ceil(row.total / limit)
     sql = "select * from todo"
     if (params.length > 0) {
       sql += ` where ${params.join(' and ')}`
     }
-
     sql += ` limit ? offset ? `
-    console.log(sql)
     db.all(sql, [limit, offset], (err, rows) => {
       if (err) return res.send(err)
-      res.render('list', { data: rows, page, pages, query: req.query, url, user: req.session.user});
+      res.render('list', { data: rows, page, pages, query: req.query, url, user: req.session.user });
     })
   })
 })
 
-router.get('/add',isLoggedIn, function (req, res) {
+router.get('/add', isLoggedIn, function (req, res) {
   res.render('add')
 })
 
@@ -128,7 +125,7 @@ router.post('/add', function (req, res) {
   })
 })
 
-router.get('/delete/:id',isLoggedIn, function (req, res) {
+router.get('/delete/:id', isLoggedIn, function (req, res) {
   const id = req.params.id
   db.run('delete from todo where id = ?', [Number(id)], (err) => {
     if (err) return res.send(err)
@@ -136,7 +133,7 @@ router.get('/delete/:id',isLoggedIn, function (req, res) {
   })
 })
 
-router.get('/edit/:id',isLoggedIn, function (req, res) {
+router.get('/edit/:id', isLoggedIn, function (req, res) {
   const id = req.params.id
   db.get('select * from todo where id = ?', [Number(id)], (err, item) => {
     if (err) return res.send(err)
@@ -144,36 +141,36 @@ router.get('/edit/:id',isLoggedIn, function (req, res) {
   })
 })
 
-router.post('/edit/:id',isLoggedIn, function (req, res) {
+router.post('/edit/:id', isLoggedIn, function (req, res) {
   const id = Number(req.params.id)
   const task = req.body.task
   const complete = JSON.parse(req.body.complete)
   if (!req.files || Object.keys(req.files).length === 0) {
-  db.run('update todo set task = ?, complete = ? where id = ?', [task, complete, id], (err, row) => {
-    if (err) return res.send(err)
-    res.redirect('/')
-  })
-}else{
-  const file = req.files.picture;
-  const fileName = `${Date.now()}-${file.name}`
-  uploadPath = path.join(__dirname, "..", "public", 'images', fileName)
+    db.run('update todo set task = ?, complete = ? where id = ?', [task, complete, id], (err, row) => {
+      if (err) return res.send(err)
+      res.redirect('/')
+    })
+  } else {
+    const file = req.files.picture;
+    const fileName = `${Date.now()}-${file.name}`
+    uploadPath = path.join(__dirname, "..", "public", 'images', fileName)
 
-  // Use the mv() method to place the file somewhere on your server
-  file.mv(uploadPath, function(err) {
-    if (err)
-      return res.status(500).send(err);
+    // Use the mv() method to place the file somewhere on your server
+    file.mv(uploadPath, function (err) {
+      if (err)
+        return res.status(500).send(err);
       db.run('update todo set task = ?, complete = ?, picture = ? where id = ?', [task, complete, fileName, id], (err, row) => {
-    res.redirect('/');
+        res.redirect('/');
       })
-  });
-}
+    });
+  }
 })
 
-router.get('/upload', function(req, res){
+router.get('/upload', function (req, res) {
   res.render('upload')
 })
 
-router.post('/upload', function(req, res) {
+router.post('/upload', function (req, res) {
   let file;
   let uploadPath;
 
@@ -186,7 +183,7 @@ router.post('/upload', function(req, res) {
   uploadPath = path.join(__dirname, "..", "public", 'images', "avatar.jpg")
 
   // Use the mv() method to place the file somewhere on your server
-  file.mv(uploadPath, function(err) {
+  file.mv(uploadPath, function (err) {
     if (err)
       return res.status(500).send(err);
 
